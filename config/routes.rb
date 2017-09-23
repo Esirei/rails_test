8291aa841fbd299c4d747ef6f10bdc043bd92720
Esi're <esiri92@gmail.com>
2017-09-23 12:54:28 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # made home the root directory
+  root 'pages#home'
+
+  # unnecessary since it's now the root
+  get 'pages/home'
+
+  # about path automatically picks controller action
+  get 'pages/about'
+
+  # about path, linked with controller action, doesn't show controller name :)
+  get 'about', to: 'pages#about'
+end
