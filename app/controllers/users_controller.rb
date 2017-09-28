28d148827e963b45c17366b0875e237d335a8bad
Esi're <esiri92@gmail.com>
2017-09-28 14:03:53 +0000

    log in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to The Rails Framework #{@user.username}"
-      redirect_to(article_path)
+      redirect_to(user_path(@user))
     else
       render 'new'
     end
