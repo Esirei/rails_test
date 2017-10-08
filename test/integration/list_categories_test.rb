c7868abc4d1c700055d62b89956635a343e35036
Esi're <esiri92@gmail.com>
2017-10-08 02:40:01 +0000

    Fixed list categories test bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ListCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @category = Category.create(name: 'books')
+    @category = Category.create(name: 'sports')
     @category2 = Category.create(name: 'programming')
   end
 
@@ -12,22 +12,6 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     assert_select 'a[href=?]', category_path(@category), text: @category.name
     assert_select 'a[href=?]', category_path(@category2), text: @category2.name
-    assert_difference 'Category.count', 1 do
-      post_via_redirect categories_path, category: { name: 'sports' }
-    end
-    assert_template 'categories/index'
-    assert_match 'sports', response.body
-  end
-
-  test 'invalid category submission results in failure' do
-    get new_category_path
-    assert_template 'categories/new'
-    assert_no_difference 'Category.count' do
-      post categories_path, category: { name: '' }
-    end
-    assert_template 'categories/index'
-    assert_select 'h2.panel-title' #from shared errors partial
-    assert_select 'div.panel-body'
   end
 
 end
